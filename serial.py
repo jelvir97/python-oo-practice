c102a935120de9e4685c92d80993114460f39f9c
Juan Elvir <jelvir97@gmail.com>
2023-05-15 09:29:21 -0600

    defined reset method on SerialGenerator class

--- serial.py
+++ serial.py
@@ -21,13 +21,19 @@ class SerialGenerator:
     """
     counter = 0
     def __init__(self,start=100):
-        self.start
+        self.start = start
 
     def generate(self):
         """Generates next sequential serial number"""
         srl_num = self.start + self.counter
         self.counter += 1
         return srl_num
+    
+    def reset(self):
+        """Resets counter back to 0. Subsequent generated serial numbers should start at starter value"""
+        self.counter = 0
+
+    
     
     
     
